4c72eab71de7965c131544013a7779352b00a63d
CarolinaSantana16 <114713240+CarolinaSantana16@users.noreply.github.com>
2022-10-05 07:52:18 -0500

    Actividades

---
+++ hoy.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoy</title>
+</head>
+<style>
+    body{
+    background-color: lightcoral;
+
+    font-family: Comic Sans MS,Arial,Verdana;
+    font-size: 45px;
+    color: #black;
+    text-align: center;
+    width: 500px;
+padding: 30px;
+margin: auto;
+border-radius: 5px;
+font-family: Comic Sans MS,Arial,Verdana ;
+font-size:60px;
+color: ::-moz-list-bullet;
+text-align: center;
+}
+<body>
+<center>
+<?php
+echo 'ACTIVIDAD 2';
+echo '<br>';
+
+echo date('d-m-y');
+?></center>
+</body>
+</html>
+
