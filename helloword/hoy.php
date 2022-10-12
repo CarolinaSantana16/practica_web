cd0f5156c74fea149f162b418ede9e965c9e4ef9
CarolinaSantana16 <114713240+CarolinaSantana16@users.noreply.github.com>
2022-10-12 17:46:19 -0500

    Actividades

---
+++ helloword/hoy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="hoy.css">
+    <title>Fecha</title>
+</head>
+<body>
+    <?php
+    echo "Godinez Santana Carolina 5 PM";
+    echo "<h2>La Fecha de hoy es</h2>";
+    echo date ('d-m-y');
+    ?>
+</body>
+</html>
